2919880e13470e1a334ebab61dca4307cb9b6e70
tuhuser <tuhvargasmora@gmail.com>
2025-05-29 14:15:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const urlParams = new URLSearchParams(window.location.search);
     const studentId = urlParams.get("id");
 
-    fetch("https://docs.google.com/spreadsheets/d/TU_ID/export?format=csv")
+    fetch("https://docs.google.com/spreadsheets/d/Id/export?format=csv")
         .then(response => response.text())
         .then(data => {
             let rows = data.split("\n");
             rows.forEach(row => {
                 let cols = row.split(",");
                 if (cols[0] == studentId) {
-                    document.getElementById("student-photo").src = cols[5]; // URL de la foto
+                    document.getElementById("student-photo").src = cols[6]; // URL de la foto
                     document.getElementById("student-name").innerText = cols[2]; // Nombre
                 }
             });
