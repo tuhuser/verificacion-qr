5be2ceb89de47a887fb5d326d60082974b59d4d1
tuhuser <tuhvargasmora@gmail.com>
2025-05-30 10:06:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const urlParams = new URLSearchParams(window.location.search);
     const studentId = urlParams.get("id");
 
-    fetch("https://docs.google.com/spreadsheets/d/1N7I1JPB2g_diEI0LzdqrzQzTmnLYFkBVZyYFTSgEVwY/edit?gid=0#gid=0/export?format=csv")
+    fetch("https://docs.google.com/spreadsheets/d/1N7I1JPB2g_diEI0LzdqrzQzTmnLYFkBVZyYFTSgEVwY/export?format=csv")
         .then(response => response.text())
         .then(data => {
             let rows = data.split("\n");
             rows.forEach(row => {
                 let cols = row.split(",");
-                if (cols[0] == studentId) { // La columna ID está en la posición 0
-                    document.getElementById("student-photo").src = cols[4]; // Enlace de la foto
-                    document.getElementById("student-name").innerText = cols[1]; // Nombre
+                if (cols[0] == studentId) {
+                    document.getElementById("student-photo").src = cols[4]; // Enlace a la foto
+                    document.getElementById("student-name").innerText = cols[1]; // Nombre del estudiante
                 }
             });
         });
